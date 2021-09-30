003e9b25b1b3882e602f4f6b8b7bdb964ef42831
Евгений Сычёв <sycheved@msk.bcs.ru>
2021-09-30 20:33:02 +0300

    Initial Commit

---
+++ UIKitVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitVK
+//
+//  Created by Евгений Сычёв on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
